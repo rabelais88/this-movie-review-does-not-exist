e18c69376066ed3a9975659b3149b0d90be9be69
rabelais88 <sungryeolp@gmail.com>
2024-05-01 15:38:50 +0700

    fix: build issue, missing vercel import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
