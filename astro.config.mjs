a9de5678bf423fe78ea18192339ce3db80d16303
rabelais88 <sungryeolp@gmail.com>
2024-05-01 18:21:15 +0700

    fix:astro vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 // import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
 import fs from 'fs';
 import path from 'path';
-
+import vercel from '@astrojs/vercel/serverless';
 const SITE_URL = 'https://www.thismoviereviewdoesnotexist.com';
 const reviewFiles = fs.readdirSync(path.resolve('./src/content/review'));
 const reviewNames = reviewFiles
@@ -16,23 +15,21 @@ const urls = [
   ...reviewNames.map((reviewName) => `${SITE_URL}/review/${reviewName}`),
   ...reviewNames.map((reviewName) => `${SITE_URL}/review/rt/${reviewName}`),
 ];
+
 // https://astro.build/config
 export default defineConfig({
   // for vercel analytics
-  output: 'server',
+  output: 'hybrid',
   // site name for astrojs/sitemap
   site: SITE_URL,
   integrations: [
-    tailwind(),
-    react(),
     sitemap({
       customPages: [...urls],
     }),
+    tailwind(),
+    react(),
   ],
-  adaptor: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
+  adapter: vercel({
+    webAnalytics: { enabled: true },
   }),
-  adapter: vercel(),
 });
